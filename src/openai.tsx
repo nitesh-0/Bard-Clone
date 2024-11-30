c744aa5252224081950c26699f9607bb98a82a38
nitesh-0 <niteshsah760@gmail.com>
2024-11-30 22:58:19 +0545

    abstracted secrets

--- src/openai.tsx
+++ src/openai.tsx
@@ -1,12 +1,24 @@
-import {GoogleGenerativeAI} from "@google/generative-ai"
+import { GoogleGenerativeAI } from "@google/generative-ai";
 
-export async function SendMessageToOpenAi(input: string){
-  console.log("inside SendMessageToOpenAiApi")
-  const genAI = new GoogleGenerativeAI("AIzaSyADZ9aFkkrMfJ9N6bDd22CuWk0JQwfsxhc");
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+export async function SendMessageToOpenAi(input: string) {
+  console.log("Inside SendMessageToOpenAiApi");
+  const apiKey = import.meta.env.VITE_OPENAI_SECRET || "";
 
-const result = await model.generateContent(input);
-console.log(result.response.text());
+  // const apiKey = "AIzaSyADZ9aFkkrMfJ9N6bDd22CuWk0JQwfsxhc"
+  console.log("API Key:", process.env.REACT_APP_OPENAI_SECRET);
+  if (!apiKey) {
+    throw new Error("API key is not defined. Check your environment variables.");
+  }
 
-return result.response.text()
+  const genAI = new GoogleGenerativeAI(apiKey);
+  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+  try {
+    const result = await model.generateContent(input);
+    console.log(result.response.text());
+    return result.response.text();
+  } catch (error) {
+    console.error("Error generating content:", error);
+    throw error;
+  }
 }
